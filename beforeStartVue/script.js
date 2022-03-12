e6b5d94c80ab1e02c55d7dbac025ea1ef74eb1bf
syrovezhko nikolai <syrovezhk@list.ru>
2022-03-12 13:00:54 +0300

    DRY buttons light control test

--- beforeStartVue/script.js
+++ beforeStartVue/script.js
@@ -1,39 +1,60 @@
-let flor1 = document.getElementById("1");
-let flor2 = document.getElementById("2");
-let flor3 = document.getElementById("3");
-let flor4 = document.getElementById("4");
-let flor5 = document.getElementById("5");
-
+// muvig gondola function 
 async function timer(id) {
-  console.log(`button ${id} has clicked`);
-  console.log(`set timer for ${id} sec`);
-  let m = 1;
-  while (id > 0) {
+  console.log(`set timer for ${id} sec after button ${id} has clicked`);
+  for (let m = id; m > 0; m--) {
     await new Promise(resolve => setTimeout(resolve, 1000));
-    console.log(m);
-    m++;
-    id --;
-  }
+    console.log(`${m}`, `sec has passed after button ${id} has clicked`);
+  };
   console.log("done");
 }
 
-flor1.addEventListener("click", function() {
+// click handling
+document.querySelectorAll('.button').forEach(item => {
+  item.addEventListener("click", function() {
   this.classList.add("button-active");
   timer(this.id).then(() => this.classList.remove("button-active"));
 });
-flor2.addEventListener("click", function() {
-  this.classList.add("button-active");
-  timer(this.id).then(() => this.classList.remove("button-active"));
-});
-flor3.addEventListener("click", function() {
-  this.classList.add("button-active");
-  timer(this.id).then(() => this.classList.remove("button-active"));
-});
-flor4.addEventListener("click", function() {
-  this.classList.add("button-active");
-  timer(this.id).then(() => this.classList.remove("button-active"));
-});
-flor5.addEventListener("click", function() {
-  this.classList.add("button-active");
-  timer(this.id).then(() => this.classList.remove("button-active"));
-});
+})
+
+/*----------------
+-first iteration-
+----------------*/
+// let flor1 = document.getElementById("1");
+// let flor2 = document.getElementById("2");
+// let flor3 = document.getElementById("3");
+// let flor4 = document.getElementById("4");
+// let flor5 = document.getElementById("5");
+
+// async function timer(id) {
+//   console.log(`button ${id} has clicked`);
+//   console.log(`set timer for ${id} sec`);
+//   let m = 1;
+//   while (id > 0) {
+//     await new Promise(resolve => setTimeout(resolve, 1000));
+//     console.log(m);
+//     m++;
+//     id --;
+//   }
+//   console.log("done");
+// }
+
+// flor1.addEventListener("click", function() {
+//   this.classList.add("button-active");
+//   timer(this.id).then(() => this.classList.remove("button-active"));
+// });
+// flor2.addEventListener("click", function() {
+//   this.classList.add("button-active");
+//   timer(this.id).then(() => this.classList.remove("button-active"));
+// });
+// flor3.addEventListener("click", function() {
+//   this.classList.add("button-active");
+//   timer(this.id).then(() => this.classList.remove("button-active"));
+// });
+// flor4.addEventListener("click", function() {
+//   this.classList.add("button-active");
+//   timer(this.id).then(() => this.classList.remove("button-active"));
+// });
+// flor5.addEventListener("click", function() {
+//   this.classList.add("button-active");
+//   timer(this.id).then(() => this.classList.remove("button-active"));
+// });
